8de8d399ad399ad6358989e3d4674126fa1092f3
Ethan <uhhhhhethan@gmail.com>
2022-11-10 13:36:23 -0800

    Clean up comments

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 // starting etch a sketch project - 11.10.22
 
-// const container = document.getElementById('container');
-// container.addEventListener('mouseover', mOverFunc);
-// container.addEventListener('mouseout', mOutFunc);
-
-// function mOverFunc() {
-//     container.style.backgroundColor = 'green';
-// }
-
-// function mOutFunc() {
-//     container.style.backgroundColor = '#d39595';
-// }
-
 // create a div within container
 // create element -> the div
 // append into container
