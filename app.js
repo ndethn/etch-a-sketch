0f9f599d5a13af905a1ad5bf1b45418ea59e715d
Ethan <uhhhhhethan@gmail.com>
2022-11-10 13:20:31 -0800

    Set div square id and style it

--- app.js
+++ app.js
@@ -19,7 +19,7 @@
 const mainContainer = document.getElementById('container');
 
 const aDiv = document.createElement('div');
-aDiv.classList.add('divSq');
+aDiv.setAttribute('id', 'divSq');
 aDiv.textContent = 'this is a div';
 
 mainContainer.appendChild(aDiv);
