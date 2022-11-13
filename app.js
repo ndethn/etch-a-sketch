6f18c0246542fa3dabb72559739901cfa689e912
Ethan <uhhhhhethan@gmail.com>
2022-11-13 12:44:06 -0800

    Create button to add a new div into main container

--- app.js
+++ app.js
@@ -4,9 +4,21 @@
 // create element -> the div
 // append into container
 
+// create main container
 const mainContainer = document.getElementById('container');
 
+// create div inside container
 const aDiv = document.createElement('div');
 aDiv.setAttribute('id', 'divSq');
-
 mainContainer.appendChild(aDiv);
+
+// create button to add new div inside main container
+const button = document.getElementById('button');
+button.addEventListener('click', addDiv);
+
+// create new div
+function addDiv() {
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('id','newDiv');
+    mainContainer.appendChild(newDiv);
+}
