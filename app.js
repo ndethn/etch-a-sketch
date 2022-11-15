54816a1871470d6ed8350d7d68c88927a277748e
Ethan <uhhhhhethan@gmail.com>
2022-11-15 15:00:35 -0800

    Style container to grid, and create 16 x 16 grid function with proper for loop

--- app.js
+++ app.js
@@ -7,22 +7,21 @@
 // create main container
 const mainContainer = document.getElementById('container');
 
-// create div inside container
-const aDiv = document.createElement('div');
-aDiv.setAttribute('id', 'divSq');
-mainContainer.appendChild(aDiv);
-
 // create button to add new div inside main container
 const button = document.getElementById('button');
 button.addEventListener('click', addDiv);
 
 // create a 16 row grid
 function addDiv() {
-    let rowMax = 16;
-    for (let i = 0; i < rowMax.length; i++) {
-        const newDiv = document.createElement('div');
-        newDiv[i].setAttribute('id','newDiv');
-        mainContainer.appendChild(newDiv[i]);
-        newDiv[i].textContent = 'hi';
+    let num = 16;
+
+    // style grid cols
+    mainContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+
+    // iterate num of divs
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('id', 'newDiv');
+        container.appendChild(square);
     }
-}
+};
